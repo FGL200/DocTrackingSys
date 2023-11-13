f5510f859703825c0ca0d15505276e41c42c9aa4
Patrick Penaranda <patrickjabonillo.26@gmail.com>
2023-11-13 18:58:39 +0800

    fixing realtime update for the line graph.

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -37,65 +37,45 @@ async function setupEncoded_monthly() {
     
 
 
-    let date = new Date();
-    date.setHours(0, 0, 0, 0);
-    let value = 100;
+    // let date = new Date();
+    // date.setHours(0, 0, 0, 0);
+    // let value = 100;
 
-    function generateData() {
-        value = Math.round((Math.random() * 10 - 4.2) + value);
-        am5.time.add(date, "month", 1);
-        am5.time.add(date, "month", 1);
-        return {
-            date: date.getTime(),
-            value: value
-        };
-    }
+    // function generateData() {
+    //     value = Math.round((Math.random() * 10 - 4.2) + value);
+    //     am5.time.add(date, "month", 1);
+    //     am5.time.add(date, "month", 1);
+    //     return {
+    //         date: date.getTime(),
+    //         value: value
+    //     };
+    // }
 
-    function generateDatas(count) {
-        let data = [];
-        for (let i = 0; i < count; ++i) {
-            data.push(generateData());
-        }
-        return data;
-    }
+    // function generateDatas(count) {
+    //     let data = [];
+    //     for (let i = 0; i < count; ++i) {
+    //         data.push(generateData());
+    //     }
+    //     return data;
+    // }
     // Edit by patrick
 
-    const months = [] // months that has number of encodes.
-
-    const encoders = [];
-    const dates = []; // dates that encoded by user.
+    // const encoders = [];
+    // const dates = []; // dates that encoded by user.
    
-    let users = await fetch_data('user/monthly/encodes', {method : 'post', form : ""});
-    // console.log(users);
-    for(let user of users){
-        if(!encoders.includes(user.uname)) encoders.push(user.uname);
-        const d = {date : new Date(user.date).getTime(), value : parseInt(user.total)};
-        if(!dates[encoders.indexOf(user.uname)]) dates[encoders.indexOf(user.uname)] = [d];
-        else dates[encoders.indexOf(user.uname)].push(d);
+    // let users = await fetch_data('user/monthly/encodes', {method : 'post', form : ""});
+    // // console.log(users);
+    // for(let user of users){
+    //     if(!encoders.includes(user.uname)) encoders.push(user.uname);
+    //     const d = {date : new Date(user.date).getTime(), value : parseInt(user.total)};
+    //     if(!dates[encoders.indexOf(user.uname)]) dates[encoders.indexOf(user.uname)] = [d];
+    //     else dates[encoders.indexOf(user.uname)].push(d);
         
-        if(!months.includes(new Date(user.date).getMonth())) months.push(new Date(user.date).getMonth());
-        if(dates[encoders.indexOf(user.uname)].length > 1) dates[encoders.indexOf(user.uname)].sort((a,b)=>a.date-b.date); // sort the dates encoded by user
+    //     if(dates[encoders.indexOf(user.uname)].length > 1) dates[encoders.indexOf(user.uname)].sort((a,b)=>a.date-b.date); // sort the dates encoded by user
 
-    }
-
+    // }
 
     // console.log(users);
- 
-    function setUpSeries(chart, name) {
-        // console.log(encoders[name])
-        return chart.series.push(am5xy.LineSeries.new(root, {
-            name: encoders[name],
-            xAxis: xAxis,
-            yAxis: yAxis,
-            valueYField: "value",
-            valueXField: "date",
-            legendValueText: "{valueY}",
-            tooltip: am5.Tooltip.new(root, {
-                pointerOrientation: "horizontal",
-                labelText: "{valueY}"
-            })
-        }));
-    }
 
     // Create axes
     // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
@@ -118,100 +98,182 @@ async function setupEncoded_monthly() {
 
     // Add series
     // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
-    for (let i = 0; i < encoders.length; i++) {
-        series = setUpSeries(chart, i);        
 
-        date = new Date();
-        date.setHours(0, 0, 0, 0);
-        value = 0;
-
-        let data =  dates[i] //generateDatas(10);
-        series.data.setAll(data);
+    function addSeries(dates, encoders) {
         
-        // Make stuff animate on load
-        // https://www.amcharts.com/docs/v5/concepts/animations/
-        series.appear();
-    }
-    
-    
-    // Add cursor
-    // https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
-    let cursor = chart.set("cursor", am5xy.XYCursor.new(root, {
-        behavior: "none"
-    }));
-    cursor.lineY.set("visible", false);
+        function setUpSeries(chart, name) {
+            // console.log(encoders[name])
+            return chart.series.push(am5xy.LineSeries.new(root, {
+                name: encoders[name],
+                xAxis: xAxis,
+                yAxis: yAxis,
+                valueYField: "value",
+                valueXField: "date",
+                legendValueText: "{valueY}",
+                tooltip: am5.Tooltip.new(root, {
+                    pointerOrientation: "horizontal",
+                    labelText: "{valueY}"
+                })
+            }));
+            
+        }
+        
+        function setUpLabels() {
+            // Add cursor
+            // https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
+            let cursor = chart.set("cursor", am5xy.XYCursor.new(root, {
+                behavior: "none"
+            }));
+            cursor.lineY.set("visible", false);
 
 
-    // Add scrollbar
-    // https://www.amcharts.com/docs/v5/charts/xy-chart/scrollbars/
-    chart.set("scrollbarX", am5.Scrollbar.new(root, {
-        orientation: "horizontal"
-    }));
+            // Add scrollbar
+            // https://www.amcharts.com/docs/v5/charts/xy-chart/scrollbars/
+            chart.set("scrollbarX", am5.Scrollbar.new(root, {
+                orientation: "horizontal"
+            }));
 
-    chart.set("scrollbarY", am5.Scrollbar.new(root, {
-        orientation: "vertical"
-    }));
+            chart.set("scrollbarY", am5.Scrollbar.new(root, {
+                orientation: "vertical"
+            }));
 
 
-    // Add legend
-    // https://www.amcharts.com/docs/v5/charts/xy-chart/legend-xy-series/
-    let legend = chart.rightAxesContainer.children.push(am5.Legend.new(root, {
-        width: 200,
-        paddingLeft: 15,
-        height: am5.percent(100)
-    }));
+            // Add legend
+            // https://www.amcharts.com/docs/v5/charts/xy-chart/legend-xy-series/
+            let legend = chart.rightAxesContainer.children.push(am5.Legend.new(root, {
+                width: 200,
+                paddingLeft: 15,
+                height: am5.percent(100)
+            }));
 
-    // When legend item container is hovered, dim all the series except the hovered one
-    legend.itemContainers.template.events.on("pointerover", function (e) {
-        let itemContainer = e.target;
+            // When legend item container is hovered, dim all the series except the hovered one
+            legend.itemContainers.template.events.on("pointerover", function (e) {
+                let itemContainer = e.target;
 
-        // As series list is data of a legend, dataContext is series
-        let series = itemContainer.dataItem.dataContext;
+                // As series list is data of a legend, dataContext is series
+                let series = itemContainer.dataItem.dataContext;
 
-        chart.series.each(function (chartSeries) {
-            if (chartSeries != series) {
-                chartSeries.strokes.template.setAll({
-                    strokeOpacity: 0.15,
-                    stroke: am5.color(0x000000)
+                chart.series.each(function (chartSeries) {
+                    if (chartSeries != series) {
+                        chartSeries.strokes.template.setAll({
+                            strokeOpacity: 0.15,
+                            stroke: am5.color(0x000000)
+                        });
+                    } else {
+                        chartSeries.strokes.template.setAll({
+                            strokeWidth: 3
+                        });
+                    }
+                })
+            })
+
+            // When legend item container is unhovered, make all series as they are
+            legend.itemContainers.template.events.on("pointerout", function (e) {
+                let itemContainer = e.target;
+                let series = itemContainer.dataItem.dataContext;
+
+                chart.series.each(function (chartSeries) {
+                    chartSeries.strokes.template.setAll({
+                        strokeOpacity: 1,
+                        strokeWidth: 1,
+                        stroke: chartSeries.get("fill")
+                    });
                 });
-            } else {
-                chartSeries.strokes.template.setAll({
-                    strokeWidth: 3
-                });
-            }
-        })
-    })
+            })
 
-    // When legend item container is unhovered, make all series as they are
-    legend.itemContainers.template.events.on("pointerout", function (e) {
-        let itemContainer = e.target;
-        let series = itemContainer.dataItem.dataContext;
-
-        chart.series.each(function (chartSeries) {
-            chartSeries.strokes.template.setAll({
-                strokeOpacity: 1,
-                strokeWidth: 1,
-                stroke: chartSeries.get("fill")
+            legend.itemContainers.template.set("width", am5.p100);
+            legend.valueLabels.template.setAll({
+                width: am5.p100,
+                textAlign: "right"
             });
-        });
-    })
 
-    legend.itemContainers.template.set("width", am5.p100);
-    legend.valueLabels.template.setAll({
-        width: am5.p100,
-        textAlign: "right"
-    });
+            // It's is important to set legend data after all the events are set on template, otherwise events won't be copied
+            legend.data.setAll(chart.series.values);
+        }
 
-    // It's is important to set legend data after all the events are set on template, otherwise events won't be copied
-    legend.data.setAll(chart.series.values);
+        // this is the function from stackoverflow
+        function updateGraph(chart, data) {
+            //New data array
+            var NewChartData = [];
+
+            //Adding new data to array
+            NewChartData.push(JSON.parse(data));
+
+            //Setting the new data to the graph
+            chart.dataProvider = NewChartData;
+
+            //Updating the graph to show the new data
+            chart.validateData();
+        }
+        
 
 
-    
-    console.log(chart.series);
+        const seriesEncoders = [];
+
+        chart.series.each(series=>{
+            seriesEncoders.push(series.get("name"));
+        })
+
+
+        console.log(seriesEncoders);
+        for (let i = 0; i < encoders.length; i++) {
+            if(seriesEncoders.indexOf(encoders[i])>=0) {
+                console.log(seriesEncoders.indexOf(encoders[i]), " ", encoders[i]);
+                continue;
+            } else {
+                if(chart.series.length < 1) {
+                    series = setUpSeries(chart, i);        
+            
+                    date = new Date();
+                    date.setHours(0, 0, 0, 0);
+                    value = 0;
+            
+                    let data =  dates[i] //generateDatas(10);
+                    series.data.setAll(data);
+                } else {
+                    updateGraph(chart, dates[])
+                }
+            }
+        }    
+
+        
+    }
 
     // Make stuff animate on load
     // https://www.amcharts.com/docs/v5/concepts/animations/
-    chart.appear(1000, 100);
+    let isDisplay = false;
+    
+
+    setInterval(async ()=>{ 
+        const encoders = [];
+        const dates = []; // dates that encoded by user.
+        let users = await fetch_data('user/monthly/encodes', {method : 'post', form : ""});
+        // console.log(users);
+        for(let user of users){
+            if(!encoders.includes(user.uname)) encoders.push(user.uname);
+            const d = {date : new Date(user.date).getTime(), value : parseInt(user.total)};
+            if(!dates[encoders.indexOf(user.uname)]) dates[encoders.indexOf(user.uname)] = [d];
+            else dates[encoders.indexOf(user.uname)].push(d);
+            
+            if(dates[encoders.indexOf(user.uname)].length > 1) dates[encoders.indexOf(user.uname)].sort((a,b)=>a.date-b.date); // sort the dates encoded by user
+
+        }
+    
+
+        addSeries(dates, encoders);
+
+
+        if(!isDisplay) {
+            isDisplay = true;
+            chart.appear(1000, 100);
+        }
+
+    }, 3000)
+
+
+
+
+    
 }
 
 async function setupEncoded_live() {
