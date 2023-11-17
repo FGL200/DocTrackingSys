7dea1d7bb13f7a0dac24de9e67eb9faa7da5c272
FGL200 <2020-100627@rtu.edu.ph>
2023-11-17 13:39:26 +0800

    query for shelves information

--- a_SQL/alter_SQL7.sql
+++ a_SQL/alter_SQL7.sql
@@ -1,5 +1,3 @@
-SET GLOBAL FOREIGN_KEY_CHECKS=0;
-
 -- STRUCTURE OF `user` TABLE
 ALTER TABLE `user` 
     ADD `created_by` INT(10) NOT NULL,
@@ -10,9 +8,45 @@ ALTER TABLE `user`
     ADD `updated_date` DATETIME NULL,
     ADD `deleted_flag` INT(1) DEFAULT 0;
 
+UPDATE `user`
+    SET `created_by` = '1'
+
 -- `user_info`
 ALTER TABLE `user_info` 
     ADD `updated_by` INT(10) NULL AFTER `gender`, 
     ADD `updated_date` DATETIME NULL DEFAULT NULL AFTER `updated_by`;
 
-SET GLOBAL FOREIGN_KEY_CHECKS=1;
+
+SELECT 
+(SELECT
+	COUNT(*)
+FROM `stud_rec` r
+INNER JOIN `doc` d
+	ON `d`.`stud_rec_id` = r.id
+WHERE 
+	r.deleted_flag = 0 AND 
+    d.shelf = '1') 'users',
+(
+    SELECT 
+        COUNT(*)
+    FROM (
+        SELECT 
+            *
+        FROM (
+            SELECT 
+                `created_by_uid` `enc`
+            FROM `stud_rec`
+            INNER JOIN `doc`
+                on doc.stud_rec_id = stud_rec.id
+            WHERE stud_rec.deleted_flag = 0
+            UNION
+            SELECT 
+                `updated_by_uid` `enc`
+            FROM `stud_rec`
+            INNER JOIN `doc`
+                on doc.stud_rec_id = stud_rec.id
+            WHERE stud_rec.deleted_flag = 0
+        ) 
+        GROUP BY `enc`
+    )
+) 'encoders'
---
+++ a_SQL/shelves_query_11172023.sql
@@ -0,0 +1,44 @@
+-- This query is for the informations of the shelf
+SELECT 
+(
+    SELECT
+	    COUNT(*)
+    FROM `stud_rec` r
+    INNER JOIN `doc` d
+        ON `d`.`stud_rec_id` = r.id
+    WHERE 
+        r.deleted_flag = 0 AND 
+        d.shelf = '1'
+) 'users',
+(
+    SELECT 
+        COUNT(*)
+    FROM 
+    (
+        SELECT 
+            `tbl1`.`enc`
+        FROM 
+        (
+            SELECT 
+                `created_by_uid` `enc`
+            FROM `stud_rec`
+            INNER JOIN `doc`
+                on doc.stud_rec_id = stud_rec.id
+            WHERE stud_rec.deleted_flag = 0 AND doc.shelf = '1'
+            UNION
+            SELECT 
+                `updated_by_uid` `enc`
+            FROM `stud_rec`
+            INNER JOIN `doc`
+                on doc.stud_rec_id = stud_rec.id
+            WHERE stud_rec.deleted_flag = 0 AND doc.shelf = '1'
+        ) `tbl1`
+        GROUP BY `tbl1`.`enc`
+    ) `tbl2`
+) `encoders`,
+(
+    SELECT 
+        `updated_date`
+    FROM `stud_rec`
+    ORDER BY `updated_date` DESC LIMIT 1
+) `latest`
