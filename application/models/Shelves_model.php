2eaf1dcc9c8087ec0fb63d042ed5d01cbf4fc4e9
Patrick Penaranda <patrickjabonillo.26@gmail.com>
2024-03-02 23:56:17 +0800

    fixed error

--- application/models/Shelves_model.php
+++ application/models/Shelves_model.php
@@ -37,49 +37,26 @@ class Shelves_model extends CI_Model {
     }
 
     public function getShelvesAndInfo() {
-        $sql = '
+        $sql = "
         SELECT 
-        `sh_`.`id`,
-        `sh_`.name,
-           CASE WHEN DATE(MAX(`sr`.`created_date`)) IS NULL THEN 0
-        ELSE count(*) 
-        END "total",
-        (
-            SELECT
-            CASE
-                WHEN count(*) <= 0 THEN "--"
-                ELSE count(*)
-            END 
-            FROM (SELECT 	
-                `sh`.`name`
-            FROM `shelves` `sh`
-            INNER JOIN `doc` `d`
-                ON `d`.`shelf` = `sh`.`id`
-            INNER JOIN `stud_rec` `sr`
-                ON `sr`.`id` = `d`.`stud_rec_id`
-            INNER JOIN `user` `u`
-                ON `u`.`id` = `sr`.`created_by_uid`
-            GROUP BY `sh`.`name`, `u`.id) as t1
-            WHERE `name` = `sh_`.`name`
-        ) as "users",
-        DATE(MAX(`sr`.`created_date`)) "last date"
-    FROM `shelves` `sh_`
-    LEFT JOIN `doc` `d`
-        ON `d`.`shelf` = `sh_`.`id`
-    LEFT JOIN `stud_rec` `sr`
-        ON `sr`.`id` = `d`.`stud_rec_id`
-    LEFT JOIN `user` `u`
-        ON `u`.`id` = `sr`.`created_by_uid`
-    LEFT JOIN `user` `u2`
-        ON `u2`.`id` = `sr`.`updated_by_uid`
-    LEFT JOIN remarks rm 
-        ON rm.stud_rec_id = sr.id
-    WHERE `sr`.`deleted_flag` != "1" || `sr`.`deleted_flag` IS NULL
-    GROUP BY `sh_`.`name`';
+                sh_.id,
+                sh_.name,
+                COUNT(*) AS total,
+                COUNT(DISTINCT u.id) AS users,
+                MAX(sr.created_date) AS last_date
+            FROM shelves sh_
+            LEFT JOIN doc d ON d.shelf = sh_.id
+            LEFT JOIN stud_rec sr ON sr.id = d.stud_rec_id
+            LEFT JOIN user u ON u.id = sr.created_by_uid
+            LEFT JOIN user u2 ON u2.id = sr.updated_by_uid
+            LEFT JOIN remarks rm ON rm.stud_rec_id = sr.id
+            WHERE sr.deleted_flag != '1' OR sr.deleted_flag IS NULL
+            GROUP BY sh_.id, sh_.name";
 
        
 
         $query = $this->db->query($sql);
+        $this->db->close();
         return $query->result_array();
     }
 
