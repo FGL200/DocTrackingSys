fc4c5bb4da7383827fe992175bf67dddc11b8b55
Patrick Penaranda <69827951+patrick23-commits@users.noreply.github.com>
2024-03-10 06:57:36 +0800

    Delete rec param

--- application/config/routes.php
+++ application/config/routes.php
@@ -56,7 +56,7 @@ $route['api/student/record/move']['post'] = 'Student/moveRecord';
 $route['api/student/record/by/(:num)'] = 'Student/get_Student_Records_By/$1';
 $route['api/student/record/by/(:num)/last'] = 'Student/get_Last_Student_Records_By/$1';
 $route['api/student/filter']['post'] = 'Student/filter_search';
-$route['api/student/record/delete']['post'] = "Student/delete_Student";
+$route['api/student/record/delete/(:num)']['post'] = "Student/delete_Student/$1";
 
 /**
  * METHOD : POST
