8aab58e274296a52374773d8ee2c94e319f2eab7
FGL200 <ericlandicho20@gmail.com>
2024-03-14 11:26:11 +0800

    for optimizations

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -25,11 +25,6 @@
   <!-- Vendor CSS Files -->
   <link href="<?= base_url() ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="<?= base_url() ?>assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="<?= base_url() ?>assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="<?= base_url() ?>assets/vendor/quill/quill.snow.css" rel="stylesheet">
-  <link href="<?= base_url() ?>assets/vendor/quill/quill.bubble.css" rel="stylesheet">
-  <link href="<?= base_url() ?>assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="<?= base_url() ?>assets/vendor/simple-datatables/style.css" rel="stylesheet">
 
   <!-- jsCalendar CSS -->
   <link rel="stylesheet" href="<?= base_url('assets/third_party/jsCalendar/jsCalendar.min') ?>.css">
@@ -51,18 +46,11 @@
   <!-- DataTable JS -->
   <script src="<?= base_url() ?>assets/third_party/datatables/datatables.min.js"></script>
 
-
-
-
   <!-- Vendor JS Files -->
   <script src="<?= base_url() ?>assets/vendor/apexcharts/apexcharts.min.js" defer></script>
   <script src="<?= base_url() ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js" defer></script>
   <script src="<?= base_url() ?>assets/vendor/chart.js/chart.umd.js" defer></script>
   <script src="<?= base_url() ?>assets/vendor/echarts/echarts.min.js" defer></script>
-  <script src="<?= base_url() ?>assets/vendor/quill/quill.min.js" defer></script>
-  <script src="<?= base_url() ?>assets/vendor/simple-datatables/simple-datatables.js" defer></script>
-  <script src="<?= base_url() ?>assets/vendor/tinymce/tinymce.min.js" defer></script>
-  <script src="<?= base_url() ?>assets/vendor/php-email-form/validate.js" defer></script>
 
   <!-- Template Main JS File -->
   <script src="<?= base_url() ?>assets/js/main.js" defer></script>
